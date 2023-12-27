f33513c8f520b21366b8c662734e1c06c3095cf3
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-27 13:39:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function expandCard(panelNumber) {
+        // Reset all panels to default state
+        for (let i = 1; i <= 5; i++) {
+            const panel = document.getElementById(`panel-${i}`);
+            panel.classList.remove('active');
+        }
+
+        // Expand the clicked panel
+        const activePanel = document.getElementById(`panel-${panelNumber}`);
+        activePanel.classList.add('active');
+    }
